ceea9cf608ebe527afab9a30554740ede1edf786
Marat <125373457+HanzoR7@users.noreply.github.com>
2024-09-17 00:25:53 +0300

    Authorization 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -7,100 +7,164 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'ПР2 ПКС',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const Authorization(),
     );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
 
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  bool _rememberMe = false;
+class Authorization extends StatelessWidget {
+  const Authorization({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Авторизация', style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Center(
         child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(75.0),
+              child: Text('Авторизация',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+              child: TextField(decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                hintText: "Логин",
+                hintStyle: const TextStyle(
+                    color: Colors.grey,
+                ),
+                fillColor: Colors.grey[200],
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none
+                  )
+                ),
+
+              ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                hintText: "Пароль",
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+                fillColor: Colors.grey[200],
+                filled: true,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none
+                    )
+                ),
+
+              ),
+              ),
+            ),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Логин',
-                border: OutlineInputBorder(),
-              ),
+            Checkbox(
+                value: false,
+                onChanged: (bool? value){},
+                side: const BorderSide(color: Colors.grey, width: 1.5),
+              checkColor: Colors.black,
             ),
-            const SizedBox(height: 16),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Пароль',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Checkbox(
-                  value: _rememberMe,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _rememberMe = value ?? false;
-                    });
-                  },
+            const Text("Запомнить меня",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16.0
+              )
+            )
+          ],
+
+        ),
+        const SizedBox(
+          height: 10.0
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+          width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[800],
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                    child: const Text("Войти",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        )
+                    )
                 ),
-                const Text('Запомнить меня'),
-              ],
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                backgroundColor: Colors.blue,
               ),
-              child: const Text(
-                'Войти',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextButton(
-              onPressed: () {
-              },
-              child: const Text(
-                'Регистрация',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-              },
-              child: const Text(
-                'Восстановить пароль',
-                style: TextStyle(color: Colors.grey),
+            Container(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+              width: double.infinity,
+              child: OutlinedButton(
+                  onPressed: (){},
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.blue[700],
+                    side: BorderSide(
+                      color: Colors.blue[400]!,
+                      width: 2.0,
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: const Text("Регистрация",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      )
+                  )
               ),
             ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                  onPressed: (){},
+                  child:  Text("Восстановить пароль",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey[500],
+                        fontWeight: FontWeight.w400,
+
+                      )
+                  )
+              )
+            )
           ],
         ),
       ),
